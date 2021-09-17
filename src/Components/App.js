a3c26f5869af45c0fbf19829e66a07cf17ab512c
Wook <wooke1995@gmail.com>
2021-09-17 17:28:17 +0900

    add router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App">Test</div>;
-}
+import React, { Component } from "react";
+import Router from "Components/Router";
+import Header from "Components/Header";
 
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
+  }
+}
 export default App;
